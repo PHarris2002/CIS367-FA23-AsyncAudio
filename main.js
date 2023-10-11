75c3fb57dc36eb681e7ff7e91eab2ee26c3b207e
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2023-10-10 23:27:54 -0500

    Functionality Progress (not working) - 10/10 11:27pm

--- main.js
+++ main.js
@@ -8,40 +8,92 @@ const progressContainer = document.querySelector('.progress-container')
 const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
 
-// Song Authors
-const authors = ['hey', 'summer', 'test']
+// Songs
+const songs = ['BestPart', 'Aidan', 'Better Days']
 
 // Keep track of authors
-let authorIndex = 2
+let songIndex = 2
 
-// Initially load author info DOM
-loadAuthor(songs[authorIndex])
+// Initially load song info DOM
+loadSong(songs[songIndex])
 
 // Update song details
-function loadAuthor(author) {
-    title.innerText = author
-    audio.src = `music/${author}.mp3`
-    cover.src = `images/${author}.jpg`
+function loadSong(song) {
+    title.innerText = song
+    audio.src=`songs/${song}.mp3`
+    cover.src=`albumart/${song}.jpg`
 }
 
 function playSong() {
 musicContainer.classList.add('play')
-playBtn.querySelector('.fas').classList.remove('fa-play')
-playBtn.querySelector('.fas').classList.add('fa-pause')
+playBtn.querySelector('').classList.remove('fa-play')
+playBtn.querySelector('').classList.add('fa-pause')
+
+audio.play()
 }
 
 function pauseSong() {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('').classList.add('fa-play')
+    playBtn.querySelector('').classList.remove('fa-pause')
+}
 
+function prevSong() {
+    songIndex--
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function nextSong() {
+    songIndex++
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+function updateProgress(e) {
+   const {duration, currentTime} = e.srcElement
+   const progressPercent = (currentTime/duration) * 100
+   progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX/width) * duration
 }
 
 // Event Listeners
 playBtn.addEventListener('click', ()=> {
     const isPlaying = musicContainer.classList.contains('play')
 
-    if(isPLaying) {
+    if(isPlaying) {
         pauseSong()
         
     } else {
         playSong()
     }
 })
+
+// Change song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
